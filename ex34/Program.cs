194ba4faed3e12ff619f3bd8fa43ff79802f4948
Mari <matorina.mariya@mail.ru>
2023-06-30 00:26:07 +0300

    ex34

---
+++ ex34/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+int[] array = GetRandomArray(10, 100, 999);
+int col = ColEvenNum(array);
+Console.WriteLine($"[{String.Join(", ", array)}] -> {col}");
+
+
+
+
+
+
+int ColEvenNum(int[] arr)
+{
+    int col = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) col++;
+    }
+    return col;
+}
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
